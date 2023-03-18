8dd0b0eefe192f527b75dc329077e2ff6e427402
jesse Lloyd <jesselloyd911@gmail.com>
2023-03-18 14:05:51 -0400

    refactor add image upload

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,63 @@
 import React, { useState } from "react";
 import "./App.css";
+import "./styles.css";
 
 function Home() {
   const [image, setImage] = useState(null);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+  const [editedImage, setEditedImage] = useState("");
+
+  function handleChange(event) {
+    const { name, value, checked, type } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: type === "checkbox" ? checked : parseInt(value), // Ensured that values are integers
+      };
+    });
+  }
+
+  const handleUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   const handleClick = () => {
-    const randomImage = Math.floor(Math.random() * 15) + 1; // generates a random number between 1 and 15
-    setImage(`./personal-images/meme${randomImage}.JPG`); // sets the image path based on the random number generated
+    const randomImage = Math.floor(Math.random() * 14) + 1;
+    setImage(`./personal-images/meme${randomImage}.JPG`);
   };
 
   return (
     <div className="App">
+      <button className="Btn" onClick={handleClick}>
+        Generate
+      </button>
+
       <header className="App-header">
         {image ? (
           <img src={image} className="img" alt="random meme" />
         ) : (
-          <p>Click the button to generate a random meme!</p>
+          <p>generate a random meme!</p>
         )}
-        <button onClick={handleClick}>Generate</button>
       </header>
+
+      <p className="upload-meme">Upload your own meme</p>
+      <input
+        type="file"
+        name="uploadedImage"
+        className="btn2"
+        onChange={handleUpload}
+      />
     </div>
   );
 }
