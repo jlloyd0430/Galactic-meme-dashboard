43f7a32e802113b5d768f391da536d87322c913a
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-21 08:05:23 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Footer from "./Footer";
@@ -8,7 +8,7 @@ import Create from "./Create";
 import Browse from "./Browse";
 
 export default function App() {
-    const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
 
   function handlePasswordChange(event) {
@@ -21,19 +21,32 @@ export default function App() {
       setLoggedIn(true);
     }
   }
+
   return (
     <div className="App">
       <Navbar />
       <div className="row">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/browse" element={<Browse />} />
-        </Routes>
+        {loggedIn ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/browse" element={<Browse />} />
+          </Routes>
+        ) : (
+          <div className="login-form">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        )}
       </div>
-      {/* <div className="main-container">
-
-      </div> */}
       <div className="footer">
         <Footer />
       </div>
